eccb6bec0fad3373046cefc941e9fc1b3f69ef39
worgflores <worgflores@gmail.com>
2021-02-02 19:49:18 +0800

    server: updated

--- server/rameses-gov-treasury-report-services.jar/sql/itemaccount_liquidation_report.sql
+++ server/rameses-gov-treasury-report-services.jar/sql/itemaccount_liquidation_report.sql
@@ -1,38 +1,45 @@
 [getReceipts]
 select 
   	c.receiptid, c.controlid, c.formno, c.series, 
-	convert(c.receiptdate, date) as receiptdate, c.receiptno, tmp1.amount, 
+	convert(c.receiptdate, date) as receiptdate, c.receiptno, tmp2.amount, 
   	case when c.voided = 0 then ia.title else '***VOIDED***' end as particulars,
   	case when c.voided = 0 then c.paidby else '***VOIDED***' end as payer,
   	case when c.voided = 0 then c.paidbyaddress else '' end as payeraddress, 
 	c.paidby, c.paidbyaddress 
 from ( 
-	select ci.receiptid, ci.acctid, sum(ci.amount) as amount 
-	from collectionvoucher cv 
-		inner join vw_remittance_cashreceiptitem ci on ci.collectionvoucherid = cv.objid 
-	where cv.controldate >= $P{startdate} 
-		and cv.controldate < $P{enddate} 
-		and ci.acctid in ( ${acctids} ) 
-	group by ci.receiptid, ci.acctid 
-	union all 
-	select ci.receiptid, ci.refacctid as acctid, -sum(ci.amount) as amount 
-	from collectionvoucher cv 
-		inner join vw_remittance_cashreceiptshare ci on ci.collectionvoucherid = cv.objid 
-	where cv.controldate >= $P{startdate} 
-		and cv.controldate < $P{enddate} 
-		and ci.refacctid in ( ${acctids} ) 
-	group by ci.receiptid, ci.refacctid 
-	union all 
-	select ci.receiptid, ci.acctid, sum(ci.amount) as amount 
-	from collectionvoucher cv 
-		inner join vw_remittance_cashreceiptshare ci on ci.collectionvoucherid = cv.objid 
-	where cv.controldate >= $P{startdate} 
-		and cv.controldate < $P{enddate} 
-		and ci.acctid in ( ${acctids} ) 
-	group by ci.receiptid, ci.acctid 
-)tmp1 
-	inner join vw_remittance_cashreceipt c on c.receiptid = tmp1.receiptid 
-	inner join itemaccount ia on ia.objid = tmp1.acctid 
+	select receiptid, acctid, sum(amount) as amount 
+	from ( 
+		select ci.receiptid, ci.acctid, sum(ci.amount) as amount 
+		from collectionvoucher cv 
+			inner join vw_remittance_cashreceiptitem ci on ci.collectionvoucherid = cv.objid 
+		where cv.controldate >= $P{startdate} 
+			and cv.controldate < $P{enddate}
+			and cv.state = 'POSTED' 
+			and ci.acctid in ( ${acctids} ) 
+		group by ci.receiptid, ci.acctid 
+		union all 
+		select ci.receiptid, ci.refacctid as acctid, -sum(ci.amount) as amount 
+		from collectionvoucher cv 
+			inner join vw_remittance_cashreceiptshare ci on ci.collectionvoucherid = cv.objid 
+		where cv.controldate >= $P{startdate} 
+			and cv.controldate < $P{enddate}
+			and cv.state = 'POSTED' 
+			and ci.refacctid in ( ${acctids} ) 
+		group by ci.receiptid, ci.refacctid 
+		union all 
+		select ci.receiptid, ci.acctid, sum(ci.amount) as amount 
+		from collectionvoucher cv 
+			inner join vw_remittance_cashreceiptshare ci on ci.collectionvoucherid = cv.objid 
+		where cv.controldate >= $P{startdate} 
+			and cv.controldate < $P{enddate}
+			and cv.state = 'POSTED' 
+			and ci.acctid in ( ${acctids} ) 
+		group by ci.receiptid, ci.acctid 
+	)tmp1 
+	group by receiptid, acctid 
+)tmp2 
+	inner join vw_remittance_cashreceipt c on c.receiptid = tmp2.receiptid 
+	inner join itemaccount ia on ia.objid = tmp2.acctid 
 order by c.formno, convert(c.receiptdate, date), c.series 
 
 
@@ -44,6 +51,7 @@ from (
 		inner join vw_remittance_cashreceiptitem ci on ci.collectionvoucherid = cv.objid 
 	where cv.controldate >= $P{startdate} 
 		and cv.controldate < $P{enddate} 
+		and cv.state = 'POSTED' 
 		and ci.acctid in ( ${acctids} ) 
 	group by ci.acctid 
 	union all 
@@ -52,6 +60,7 @@ from (
 		inner join vw_remittance_cashreceiptshare ci on ci.collectionvoucherid = cv.objid 
 	where cv.controldate >= $P{startdate} 
 		and cv.controldate < $P{enddate} 
+		and cv.state = 'POSTED' 
 		and ci.refacctid in ( ${acctids} ) 
 	group by ci.refacctid 
 	union all 
@@ -60,6 +69,7 @@ from (
 		inner join vw_remittance_cashreceiptshare ci on ci.collectionvoucherid = cv.objid 
 	where cv.controldate >= $P{startdate} 
 		and cv.controldate < $P{enddate} 
+		and cv.state = 'POSTED' 
 		and ci.acctid in ( ${acctids} ) 
 	group by ci.acctid 
 )tmp1 
